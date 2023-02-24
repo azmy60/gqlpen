4f6591cf8104a6f8984b9957f045c69b768cf8ab
azmy60 <38707148+azmy60@users.noreply.github.com>
2023-02-24 10:28:30 +0700

    update theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,23 @@ module.exports = {
     plugins: [require('daisyui')],
     daisyui: {
         logs: false,
-        themes: ['forest'],
+        themes: [
+            {
+                forest: {
+                    ...require('daisyui/src/colors/themes')[
+                        '[data-theme=forest]'
+                    ],
+                    primary: '#1363DF',
+                    secondary: '#1d4ed8',
+                    accent: '#34d399',
+                    neutral: '#16181D',
+                    'base-100': '#050505',
+                    info: '#3ABFF8',
+                    success: '#36D399',
+                    warning: '#facc15',
+                    error: '#ef4444',
+                },
+            },
+        ],
     },
 };
